d076f5ed8a82eca65caee81f4692d825764881e5
Maroa <132206724+Maroahehe@users.noreply.github.com>
2025-04-15 17:03:03 -0300

    Add files via upload

---
+++ exercicio7/atv1.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<?php
+
+
+	$x = $_POST["A"];
+	$y = $_POST["B"];
+
+    
+    $n1 = $x ;
+    $n2 = $y ;
+
+	$media = $x / $y;
+	echo "A media de KM/L é $media";
+?>
+</body>
+</html>
